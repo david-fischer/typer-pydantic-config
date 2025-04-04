808bb47faac2ade0aaa76b0802f1fabd71f9f553
David Fischer <d.fischer.git@posteo.de>
2025-04-04 13:33:13 +0200

    chore: extract conversion of pydantic Field -> click Option

--- src/typer_pydantic_config/click_utils.py
+++ src/typer_pydantic_config/click_utils.py
@@ -1,9 +1,11 @@
 import types
 from collections.abc import Callable
+from itertools import starmap
 from typing import Any, Union, get_args, get_origin
 
 import click
 from pydantic import BaseModel
+from pydantic.fields import FieldInfo
 
 from .constants import ANNOTATION_MAP, FIELD_SEP
 from .dict_utils import flatten_dict
@@ -45,6 +47,23 @@ def get_flat_fields(model: type[BaseModel]) -> dict[str, Any]:
     return flatten_dict(get_nested_fields(model))
 
 
+def model_field_to_click_option(
+    field_name: str,
+    field_info: FieldInfo,
+) -> click.Option:
+    return click.Option(
+        param_decls=[
+            f"--{field_name.replace('_', '-')}",
+            field_name.replace(".", FIELD_SEP),
+        ],
+        help=field_info.description or field_name,
+        default=None,
+        required=False,
+        show_default=False,
+        type=annotation_to_click_type(field_info.annotation),
+    )
+
+
 def update_pydantic_model_command[PydanticModel: BaseModel](
     pydantic_model: type[PydanticModel],
     callback: Callable[[...], ...],
@@ -54,23 +73,11 @@ def update_pydantic_model_command[PydanticModel: BaseModel](
     The user can do:
       <myapp> config set --username new_user --api-key SECRET --timeout 60
     """
-    params = [
-        click.Option(
-            param_decls=[
-                f"--{field_name.replace('_', '-')}",
-                field_name.replace(".", FIELD_SEP),
-            ],
-            help=(field_model.description or field_name),
-            default=None,
-            required=False,
-            show_default=False,
-            type=annotation_to_click_type(field_model.annotation),
-        )
-        for field_name, field_model in get_flat_fields(pydantic_model).items()
-    ]
     return click.core.Command(
         name="set",
         help="Set one or more config fields via flags.",
         callback=callback,
-        params=params,
+        params=list(
+            starmap(model_field_to_click_option, get_flat_fields(pydantic_model))
+        ),
     )
